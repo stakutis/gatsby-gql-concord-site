ee1be4b2a0587c91a55b3730ef0eb55d60910661
iqbal125 <iqbal125@yahoo.com>
2020-05-04 17:44:39 -0700

    completed services page

--- src/components/ServicesCardSection/index.js
+++ src/components/ServicesCardSection/index.js
@@ -2,21 +2,36 @@ import React from 'react';
 import ServiceCard from '../ServicesCard';
 
 import styles from './servicecardsection.module.css';
-import { servicesWeb } from './services';
+import { servicesWeb, otherServices } from './services';
 import { FaReact } from 'react-icons/fa';
+import { FcOnlineSupport } from 'react-icons/fc';
 
 const ServiceCardSection = () => {
   return (
     <div className={styles.top_div}>
       <h1>Areas Of Expertise</h1>
       <FaReact className={styles.main_icon} />
-      <h3>
+      <h3 className={styles.title_desc}>
         We have the tech experience you need, Our expert team of advisors and consultants can answer
         your questions and design solutions
       </h3>
-      <h2> Web Development</h2>
+
+      <div className={styles.consult_card}>
+        <div className={styles.consult_icon}>
+          <FcOnlineSupport />
+        </div>
+        <p>Free consultations to discuss your software & engineering needs</p>
+      </div>
+
+      <h1> Web Development</h1>
       <div className={styles.card_rows}>
         {servicesWeb.map(service => (
+          <ServiceCard service={service} />
+        ))}
+      </div>
+      <h1 className={styles.second_header}>Other Services</h1>
+      <div className={styles.card_rows}>
+        {otherServices.map(service => (
           <ServiceCard service={service} />
         ))}
       </div>
--- src/components/ServicesCardSection/services.js
+++ src/components/ServicesCardSection/services.js
@@ -1,43 +1,67 @@
-import { FaDesktop } from 'react-icons/fa';
-import { FaServer } from 'react-icons/fa';
-import { FaMobile } from 'react-icons/fa';
-import { FaWrench } from 'react-icons/fa';
-import { FaDatabase } from 'react-icons/fa';
+import { FcSupport } from 'react-icons/fc';
+import { FcAcceptDatabase } from 'react-icons/fc';
+import { FcServices } from 'react-icons/fc';
+import { FcCurrencyExchange } from 'react-icons/fc';
+import { FcFilingCabinet } from 'react-icons/fc';
+import { FcIphone } from 'react-icons/fc';
+import { FcMultipleDevices } from 'react-icons/fc';
+import { FcSportsMode } from 'react-icons/fc';
 
 export const servicesWeb = [
   {
     id: 1,
     title: 'Front End',
-    icon: FaDesktop,
+    icon: FcMultipleDevices,
     description: ['React', 'Redux', 'Gatsby', 'HTML, CSS, Javascript', 'Auth0', 'D3']
   },
   {
     id: 2,
     title: 'Server Side',
-    icon: FaServer,
+    icon: FcFilingCabinet,
     description: ['Node.js', 'Serverless AWS Lambda', 'Java', 'REST API Design', 'Python Django']
   },
   {
     id: 3,
     title: 'DevOps',
-    icon: FaWrench,
+    icon: FcSupport,
     description: ['Docker', 'AWS Suite', 'Jenkins', 'Travis CI', 'Git', 'JWT Authentication']
   },
   {
     id: 4,
     title: 'Databases',
-    icon: FaDatabase,
+    icon: FcAcceptDatabase,
     description: ['AWS RDS Setup', 'Postgres', 'DynamoDB', 'MySQL']
   },
   {
     id: 5,
     title: 'Mobile',
-    icon: FaMobile,
+    icon: FcIphone,
     description: [
       'Responsive Web Design',
       'Progressive Web App',
       'Mobile Site Development',
       'React Native'
     ]
+  }
+];
+
+export const otherServices = [
+  {
+    id: 1,
+    title: 'Investment',
+    icon: FcCurrencyExchange,
+    description: ['Specializing in due diligence and private equity']
+  },
+  {
+    id: 2,
+    title: 'Healthcare',
+    icon: FcSportsMode,
+    description: ['Biotech', 'Pharma', 'Mobile', 'Genetics', 'Aging-in-place']
+  },
+  {
+    id: 3,
+    title: 'Engineering',
+    icon: FcServices,
+    description: ['Robotics', 'Automation', 'Process Control', 'Surveillance']
   }
 ];
